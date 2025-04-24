5151f518f302e46b341ea84acfd146c85ab4c281
Noud63 <noudvandun@gmail.com>
2025-04-24 15:31:40 +0200

    font-size

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ return (
       <div className="flex flex-row items-center">
         <Link href="/">
           <div className="mb-2 flex flex-col leading-none tracking-wide">
-            <div className="font-CloisterBlack text-[4rem] text-white max-xsm:text-[3.2rem]">
+            <div className="font-CloisterBlack text-[4rem] text-white max-xsm:text-[3.4rem]">
               Staatslieden
             </div>
             <div className="text-[0.76rem] text-[#ffcb3b]  retina:text-[0.72rem] max-xsm:text-[0.58rem]">

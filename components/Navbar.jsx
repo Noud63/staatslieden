3024cb625bf574bb50d549b6dceacfff2a8f897a
Noud63 <noudvandun@gmail.com>
2025-05-07 13:12:04 +0200

    retina

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,19 +13,19 @@ return (
   <div className="navbar flex h-[100px] w-full justify-between border-b border-yellow-800 py-3 max-sm:px-4">
     <div className="mx-auto flex w-full max-w-[1980px] flex-row items-center justify-between px-6 max-sm:justify-between max-sm:px-0">
       <div className="flex flex-row items-center">
-        <Link href={`/${locale}`}>
-          <div className="mb-2 flex flex-col leading-none">
-            <div className="font-CloisterBlack text-[4rem] text-white max-xsm:text-[3.2rem]">
+        <Link href="/">
+          <div className="mb-2 flex flex-col items-center leading-none tracking-wide">
+            <div className="w-fit text-center font-CloisterBlack text-[4rem] text-white max-xsm:text-[3.6rem]">
               Staatslieden
             </div>
-            <div className="text-[0.8rem] text-[#ffcb3b] retina:text-[0.76rem] max-xsm:text-[0.62rem]">
+            <div className="text-[0.8rem] text-[#ffcb3b] retina:text-[0.76rem] retina:max-xsm:text-[0.6rem] max-xsm:text-[0.62rem]">
               webapplicatie voor de Staatsliedenbuurt Amsterdam
             </div>
           </div>
         </Link>
       </div>
 
-      <div className="flex w-[350px] flex-row items-center justify-between max-md:justify-end">
+      <div className="flex w-[350px] flex-row items-center justify-between">
         <div className="max-md:hidden">
           <LoginRegisterLogout />
         </div>

063ffe310654f41d9fe1af11bc1db93cf51ecc3d
Noud63 <noudvandun@gmail.com>
2025-07-04 12:46:51 +0200

    Optimistic update postcomment

--- app/[locale]/postsByUserId/[postId]/layout.jsx
+++ app/[locale]/postsByUserId/[postId]/layout.jsx
@@ -3,7 +3,7 @@ import AddPost from "@/components/AddPost";
 export default function Layout({ children }) {
   return (
     <div className="w-full max-w-[670px] mx-auto">
-      <AddPost />
+      {/* <AddPost /> */}
       <main>{children}</main>
 
     </div>

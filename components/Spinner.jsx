42e56142c734aa8b63c4f6859dfc5b3f8693efa3
Noud63 <noudvandun@gmail.com>
2025-10-17 12:58:10 +0200

    SinglePost Interactivity SWR

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -2,13 +2,17 @@
 import React from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 
-
+const override = {
+  display: "block",
+  margin: "0 auto",
+};
 
 const Spinner = ({ loading, size}) => {
   return (
     <ClipLoader
       color="#fff"
       loading={loading} // loading state true or false
+      cssOverride={override}
       size={size}
       aria-label="Loading Spinner"
     />

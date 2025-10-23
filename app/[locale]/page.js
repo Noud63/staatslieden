a0c989e9ad76ada0f505ea29a8b43a1a0c658930
Noud63 <noudvandun@gmail.com>
2025-10-23 14:40:18 +0200

    deleted loading.jsx

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -16,3 +16,28 @@ export default async function Home() {
     </>
   );
 }
+
+
+// "use client"
+// import React from 'react'
+// import GridLoader from "react-spinners/GridLoader"
+
+// const override = {
+//     display: 'block',
+//     margin: '100px auto'
+    
+// }
+
+// const LoadingPage = ({loading}) => {
+//   return (
+//     <GridLoader
+//       color="#fff"
+//       loading={loading}
+//       cssOverride={override}
+//       size={10}
+//       aria-label="Loading Spinner"
+//     />
+//   );
+// }
+
+// export default LoadingPage

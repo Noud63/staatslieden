9e0d06692bba3e118b335d60fed0eb3c5182f352
Noud63 <noudvandun@gmail.com>
2025-11-11 13:57:40 +0100

    next/intl 4.5.1

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -8,27 +8,29 @@ import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { Toaster } from "react-hot-toast";
+import { hasLocale } from "next-intl";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/captions.css";
 
 // Replace hasLocale with a custom implementation
-const isValidLocale = (locales, locale) => locales.includes(locale);
+// const isValidLocale = (locales, locale) => locales.includes(locale);
 
 export default async function RootLayout({ children, params }) {
   const { locale } = await params;
 
   console.log(routing.locales, locale)
-  if (!isValidLocale(routing.locales, locale)) {
+  
+  if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
 
-  const messages = (await import(`../../messages/${locale}.json`)).default;
+  // const messages = (await import(`../../messages/${locale}.json`)).default;
 
   return (
     <html lang={locale}>
       <body className="relative bg-gradient-to-r from-red-950 via-yellow-700 to-red-950">
         <div className="fixed -z-10 h-full w-full bg-[url('/images/homebg.png')] bg-cover bg-center bg-no-repeat" />
-        <NextIntlClientProvider locale={locale} messages={messages}>
+        <NextIntlClientProvider >
           <AuthProvider>
             <Navbar />
             <div className="max-md:flex md:hidden">

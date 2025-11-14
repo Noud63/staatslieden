35e5c0f269761e96349e11c82bce2241ed17e0c9
Noud63 <noudvandun@gmail.com>
2025-11-14 09:09:15 +0100

    staatslieden items

--- app/[locale]/menu/staatslieden/page.jsx
+++ app/[locale]/menu/staatslieden/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import straatnamen from "@/data/straatnamen.json";
+import Staatslieden from "@/components/hoofdmenu/staatslieden/Staatslieden";
 
 const StaatsliedenPage = () => {
   //If you only need the href's keys from the array
@@ -7,43 +7,7 @@ const StaatsliedenPage = () => {
   // console.log("Hrefs:", hrefs)
 
   return (
-    <div className="w-full max-w-[1280px] mx-auto mt-8 px-4 text-white max-md:mt-4 max-sm:mt-4 max-sm:px-4">
-      <div className="flex justify-center border-b-2 text-2xl font-semibold tracking-wide">
-        <div className="px-4 pb-4">De Staatslieden</div>
-      </div>
-
-      <div className="mt-4 mb-6">
-        De staasliedenbuurt kent 35 straten.
-        <br />
-        Alle straatnamen zijn vernoemd naar Nederlandse staatslieden uit de 18e
-        en 19e eeuw.
-        <br />
-        Maar wie waren die hoge heren en wat was hun functie?
-        <br />
-        Hieronder een lijst van alle straatnamen.
-        <br />
-        Klik op een naam voor een korte kennismaking met de persoon naar wie de
-        straat genoemd is en de locatie op de plattegrond.
-      </div>
-
-      <div className="">
-       
-        {/* <ul className="w-full max-w-[220px] cursor-pointer list-disc space-y-2 text-yellow-900 "> */}
-          <div className="mt-4 grid grid-cols-[repeat(auto-fill,_minmax(260px,_1fr))] gap-2">
-          {straatnamen &&
-            straatnamen.map((naam) => (
-              <div
-                key={naam.naam}
-                className="rounded-md bg-white py-1 pl-2 font-semibold text-white decoration-yellow-800 hover:underline cursor-pointer"
-              >
-                {/* <a href={naam.href} target="_blank"><span className="text-yellow-900">{naam.naam}</span></a> */}
-                <span className="text-yellow-900">{naam.naam}</span>
-              </div>
-            ))}
-            </div>
-        {/* </ul> */}
-      </div>
-    </div>
+    <Staatslieden />
   );
 };
 
---
+++ components/hoofdmenu/staatslieden/Staatslieden.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import straatnamen from "@/data/straatnamen.json";
+import Image from "next/image";
+
+const staatslieden = () => {
+  //If you only need the href's keys from the array
+  // const hrefs = straatnamen.map((obj) => ({href: obj.href}))
+  // console.log("Hrefs:", hrefs)
+
+  return (
+    <div className="mx-auto mt-8 w-full max-w-[1280px] px-4 text-white max-md:mt-4 max-sm:mt-4 max-sm:px-4">
+      <div className="flex justify-center border-b-2 text-2xl font-semibold tracking-wide">
+        <div className="px-4 pb-4">De Staatslieden</div>
+      </div>
+
+      <div className="mb-6 mt-4">
+        De staasliedenbuurt kent 35 straten.
+        <br />
+        Alle straatnamen zijn vernoemd naar Nederlandse staatslieden uit de 18e
+        en 19e eeuw.
+        <br />
+        Maar wie waren die hoge heren en wat was hun functie?
+        <br />
+        Hieronder een lijst van alle straatnamen en een korte kennismaking met de
+        persoon naar wie de straat genoemd is.
+        <br />
+        Klik op het google maps icoon voor de locatie op de plattegrond.
+      </div>
+
+      <div className="">
+        <div className="mt-4 grid auto-rows-[380px] grid-cols-[repeat(auto-fill,_minmax(260px,_1fr))] gap-2">
+          {straatnamen &&
+            straatnamen.map((naam) => (
+              <div
+                key={naam.naam}
+                className="relative flex flex-col rounded-md bg-white px-3 pt-1 decoration-yellow-800"
+              >
+                <div className="mb-3 w-full border-b border-yellow-900 pb-1">
+                  <span className="text-lg font-semibold text-yellow-900">
+                    {naam.naam}
+                  </span>
+                </div>
+
+                <div className="w-[80px]">
+                  <Image src={naam.img ? naam.img : "/images/staatslieden/king.jpg"} width={100} height={0} alt=""/>
+                </div>
+
+                <div className="mt-2 text-yellow-900">{naam.desc?.slice(0, 138)}.....</div>
+
+                <div className="absolute bottom-2 mt-2 flex mb-1">
+                  <a href={naam.href} target="_blank">
+                    <Image
+                      src="/icons/googlemaps.png"
+                      width={30}
+                      height={0}
+                      alt=""
+                      className="h-auto w-[28px]"
+                    />
+                  </a>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default staatslieden;
